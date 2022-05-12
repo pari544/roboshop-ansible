c7d302468cea8259e556f0d0ccab2bc05a53b551
Win10 <yeddulaparineetha@gmail.com>
2022-05-12 23:21:41 +0530

    Updated the var.yml

--- create-ec2.sh
+++ create-ec2.sh
@@ -18,15 +18,14 @@ aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${INSTANC
 if [ $? -eq 0 ]; then
   echo "Instance already exists"
   exit 0
+else
+  AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,
+  InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=instance,
+  Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" &>/dev/null
+  echo "EC2 Instance created"
+  sleep 30
 fi
-AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
-
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,
-InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=instance,
-Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" &>/dev/null
-echo "EC2 Instance created"
-
-sleep 30
 
 INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${INSTANCE_NAME} Name=state,Values=active --output table | grep InstanceId | awk  '{print $4}')
 echo "Instance id is ${INSTANCE_ID}"
